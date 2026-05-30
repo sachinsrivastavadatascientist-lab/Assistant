9b992d4bae66f65ab9ca11db2c45fb4685b505a0
sachinsrivastavadatascientist-lab <sachinsrivastavadatascientist@gmail.com>
2026-05-30 20:53:47 +0530

    improved uiconfigfile

--- src/langgraphagenticai/ui/uiconfigfile.py
+++ src/langgraphagenticai/ui/uiconfigfile.py
@@ -1,19 +1,23 @@
 from configparser import ConfigParser
+from pathlib import Path
 
 
 class Config:
-    def __init__(self,config_file = r"src\langgraphagenticai\ui\uiconfigfile.ini"):
-        self.config = ConfigParser() # able to read config file
-        self.config.read(config_file)
+    def __init__(self):
+        self.config = ConfigParser()
 
-    def get_llm_options(self) :
-        return self.config["DEFAULT"].get('LLM_OPTIONS')
-    
-    def get_usecase_options(self) :
-        return self.config["DEFAULT"].get('USECASE_OPTIONS').split(",")
-    
-    def get_groq_model_options(self) :
-        return self.config["DEFAULT"].get('GROQ_MODEL_OPTIONS').split(",")
-    
-    def get_page_title(self) :
-        return self.config["DEFAULT"].get('PAGE_TITLE')
+        config_path = Path(__file__).parent / "uiconfigfile.ini"
+
+        self.config.read(config_path)
+
+    def get_llm_options(self):
+        return self.config["DEFAULT"].get("LLM_OPTIONS")
+
+    def get_usecase_options(self):
+        return self.config["DEFAULT"].get("USECASE_OPTIONS").split(",")
+
+    def get_groq_model_options(self):
+        return self.config["DEFAULT"].get("GROQ_MODEL_OPTIONS").split(",")
+
+    def get_page_title(self):
+        return self.config["DEFAULT"].get("PAGE_TITLE")
